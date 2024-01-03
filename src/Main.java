5e0f443ca507407bccca8e8e684219f0136e5cd5
Pranav Goyal <pr945005@dal.ca>
2024-01-03 15:23:37 +0530

    Added UC1 computing length of line.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Welcome to Line\n" +
-                "Comparison Computation\n" +
-                "Program");
+
+        /**
+         * This is UC 1 where we need to compute the length of the line given its coordinates.
+         * */
+
+        int x1, x2, y1, y2;
+        x1=2;
+        y1=3;
+        x2=7;
+        y2=9;
+
+        double length = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        System.out.println("The length of line is: " + length);
     }
 }

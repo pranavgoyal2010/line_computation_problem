71641b6be11112c27750c545d6616e70c70e1a52
Pranav Goyal <pr945005@dal.ca>
2024-01-03 16:32:35 +0530

    Added UC3 code to compare lengths of lines.

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,16 @@ public class Main {
         Point point2 = new Point(7,9);
 
         Point point3 = new Point(2,3);
-        Point point4 = new Point(7,9);
+        Point point4 = new Point(8,9);
 
+        double length1 = Math.sqrt(Math.pow(point2.getX()-point1.getX(), 2) + Math.pow(point2.getY()-point1.getY(), 2));
+        double length2 = Math.sqrt(Math.pow(point4.getX()-point3.getX(), 2) + Math.pow(point4.getY()-point3.getY(), 2));
 
-        if(point1.getX()==point3.getX() && point1.getY()==point3.getY()
-        && point2.getX()==point4.getX() && point2.getY()==point4.getY())
+        if(length1==length2)
             System.out.println("Lines are equal");
-
+        else if(length1>length2)
+            System.out.println("Line 1 is greater than Line 2");
         else
-            System.out.println("Lines are not equal");
+            System.out.println("Line 1 is lesser than Line 2");
     }
 }
